8c5c764def56a8dab367feea1dc5923cd6cdaa59
CescVilanova <vfrancesc@gmail.com>
2014-01-28 16:51:39 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripreviews::Application.config.secret_key_base = '97914eb186e6256d12f71baf480a7d32a655ad812bdd11db2f16716608f8a1ed93756ffca59979c4711f7b843c72982bd2b0385977686995ec71b79a7b6cb9d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tripreviews::Application.config.session_store :cookie_store, key: '_Tripreviews_session'
